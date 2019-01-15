8327e7c1c2ae1d004bf16b580e2f96cec162f89b
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-01-15 20:41:57 +0530

    enchilada: Initial AOSiP Bringup
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -22,9 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_enchilada
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Anirudh Gupta"
+
+PRODUCT_NAME := aosip_enchilada
 PRODUCT_DEVICE := enchilada
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
