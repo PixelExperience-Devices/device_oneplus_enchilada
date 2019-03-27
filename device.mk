e6a2a450c09fad43d98e7f0e6b4f8baf2622e44a
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2019-03-26 21:44:19 -0400

    enchilada: Add Notch Bar Killer overlay
    this overlay gets rid of the black bar in most apps/games and lets the screen fill past the notch.
    
    xlxfoxxlx: reworked for inline building and crosshatch
    anirudhgupta109: modified for enchilada dimensions
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    NoCutoutOverlay \
+    NotchBarKiller
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
