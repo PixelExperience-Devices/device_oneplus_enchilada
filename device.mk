19868f72a8b960438d71a81fa2941bb02c69daab
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-06-20 17:37:21 +0530

    enchilada: overlay: Remove NCO, NBK and commonise it
    See: https://review.aosiprom.com/c/AOSIP-Devices/device_oneplus_sdm845-common/+/8234
    Change-Id: I87919c785713e3a54c02b5ada63b3a5eb3f11939
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay \
-    NotchBarKiller
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
