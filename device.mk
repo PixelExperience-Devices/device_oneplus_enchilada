ac50534a22aad125a1518abccb27eac73583f93c
yashsonawane <yash050219991@gmail.com>
2020-01-24 11:50:24 +0000

    enchilada: clean up
    Signed-off-by: yashsonawane <yash050219991@gmail.com>

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
