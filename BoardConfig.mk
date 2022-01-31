8fb7d5455d15951a5c6ad3f132a24bd99c28e348
Michael Bestas <mkbestas@lineageos.org>
2022-01-31 03:27:21 +0200

    enchilada: Add board-info.txt
    * Fixes flashall in fastbootd
    
    Change-Id: Iae2f33113317cfaadeff7a3610463880077c35e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ DEVICE_PATH := device/oneplus/enchilada
 
 # Assertions
 TARGET_OTA_ASSERT_DEVICE := OnePlus6,oneplus6,enchilada
+TARGET_BOARD_INFO_FILE := device/oneplus/enchilada/board-info.txt
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
